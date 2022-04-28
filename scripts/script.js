f89c547f9a3b1a345272d98ec8d02be716255c86
Nicolas Berendsen <nicolasberendsen@gmail.com>
2022-04-27 17:29:24 -0700

    Create grid based on user input

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,34 @@ boxes.forEach(box =>  {
   box.addEventListener('mouseover', modify);
 })
 
-function createGrid() {
+function createGrid(width) {
+
+    const container = document.querySelector('#container');
+
+    for (let row = 0; row < width; ++row) {
+
+      const rowDiv = document.createElement('div');
+      rowDiv.style.display = 'flex';
+
+      for (let col = 0; col < width; ++col) {
+
+        const boxNumber = col + row * width;
+        const colDiv = document.createElement('div');
+        colDiv.classList.add('box');
+        colDiv.setAttribute('id', `box-${boxNumber}`);
+
+        rowDiv.appendChild(colDiv);
+      }
+
+      container.appendChild(rowDiv);
+    }
+
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box =>  {
+      box.addEventListener('mouseover', modify);
+    })
 
 }
 
-// createGrid(prompt('Dimension?'));
+createGrid(prompt('Dimension?'));
